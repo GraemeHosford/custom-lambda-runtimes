08217b426ae1fb7a25c4f35a00ef599bee09c7c3
Graeme Hosford <graemehosford@gmail.com>
2022-08-13 15:31:29 +0100

    Add code for creating a Bash runtime lambda layer + a test function

--- lib/custom-lambda-runtimes-stack.ts
+++ lib/custom-lambda-runtimes-stack.ts
@@ -1,15 +1,45 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as iam from "@aws-cdk/aws-iam";
+import {RetentionDays} from "@aws-cdk/aws-logs";
 
 export class CustomLambdaRuntimesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaExecutionRole = new iam.Role(this, "BashLambdaExecutionRole", {
+      roleName: "BashLambdaExecutionRole",
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com")
+    });
+    lambdaExecutionRole.addManagedPolicy(
+        iam.ManagedPolicy.fromManagedPolicyArn(
+            this,
+            "Lambda-Access-Role",
+            "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+        )
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CustomLambdaRuntimesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bashRuntimeLambdaLayer = new lambda.LayerVersion(this, "bash-lambda-layer", {
+      layerVersionName: "BashRuntime",
+      description: "Bash runtime for AWS lambdas",
+      compatibleRuntimes: [lambda.Runtime.PROVIDED, lambda.Runtime.PROVIDED_AL2],
+      compatibleArchitectures: [lambda.Architecture.ARM_64, lambda.Architecture.X86_64],
+      code: lambda.Code.fromAsset(`${__dirname}/runtime`),
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    bashRuntimeLambdaLayer.addPermission("BashRuntimeSharePermissions", {
+      accountId: "12345678901"
+    });
+
+    new lambda.Function(this, "BashTestFunction", {
+      layers: [bashRuntimeLambdaLayer],
+      code: lambda.Code.fromAsset(`${__dirname}/function`),
+      handler: "handler.handler",
+      runtime: lambda.Runtime.PROVIDED,
+      role: lambdaExecutionRole,
+      functionName: "BashTestFn",
+      logRetention: RetentionDays.ONE_DAY
+    });
   }
 }
